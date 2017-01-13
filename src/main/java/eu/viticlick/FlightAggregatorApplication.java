b043ef92647af4faeda41a4bd92b861cf6f6a1ff
Víctor López <vlopez.gal@gmail.com>
2017-01-13 23:16:10 +0100

    bootstrap project

---
+++ src/main/java/eu/viticlick/FlightAggregatorApplication.java
@@ -0,0 +1,12 @@
+package eu.viticlick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightAggregatorApplication.class, args);
+	}
+}
